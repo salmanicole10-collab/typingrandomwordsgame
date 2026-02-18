c0d40eb7f55308d6ddbc93a3c8a28e0488339753
salmanicole10-collab <salmanicole10@gmail.com>
2026-02-18 13:28:58 -0400

    Add random selection and counters

--- wordsgamemaster.py
+++ wordsgamemaster.py
@@ -3,4 +3,8 @@ import random
 words = words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
           "otorrinolaringologo", "new horizons", "development", "successful", "arancel", "abracadabrante"
 ]
+game_words = random.sample (words, 15)
+mistakes = 0 
+total = len(game_words)
+count = 1 
 
