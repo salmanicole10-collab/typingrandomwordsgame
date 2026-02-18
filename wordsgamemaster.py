d4b93cf90d4735b03fb215f7841ff4b374a5d37a
salmanicole10-collab <salmanicole10@gmail.com>
2026-02-18 13:57:56 -0400

    Fix 1 line

--- wordsgamemaster.py
+++ wordsgamemaster.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 words = words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
           "otorrinolaringologo", "new horizons", "development", "successful", "arancel", "abracadabrante"
