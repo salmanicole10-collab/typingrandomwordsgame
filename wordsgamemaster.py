2182ea7773cf014eadcecf52acbaddb177e27739
salmanicole10-collab <salmanicole10@gmail.com>
2026-02-18 14:02:53 -0400

    Add custom word list

--- wordsgamemaster.py
+++ wordsgamemaster.py
@@ -1,9 +1,8 @@
 import random
 
-words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
+words = words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
           "otorrinolaringologo", "new horizons", "development", "successful", "arancel", "abracadabrante"
 ]
-
 game_words = random.sample (words, 15)
 mistakes = 0 
 total = len(game_words)
