6332c796f491fde6a5fb457102acd588d48f134d
salmanicole10-collab <salmanicole10@gmail.com>
2026-02-18 13:26:07 -0400

    Add custom word list

---
+++ wordsgamemaster.py
@@ -0,0 +1,6 @@
+import random 
+
+words = words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
+          "otorrinolaringologo", "new horizons", "development", "successful", "arancel", "abracadabrante"
+]
+
