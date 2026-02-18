0d43a35c3a798e4707bcaa7d78332b0b2547ecac
salmanicole10-collab <salmanicole10@gmail.com>
2026-02-18 13:59:07 -0400

    Fixing errors

--- wordsgamemaster.py
+++ wordsgamemaster.py
@@ -1,8 +1,9 @@
 import random
 
-words = words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
+words = [ "profe braulio", "salma", "software", "valentines", "sanguijuela", "graduacion", "computadora", "abrigo", "desarrollar",
           "otorrinolaringologo", "new horizons", "development", "successful", "arancel", "abracadabrante"
 ]
+
 game_words = random.sample (words, 15)
 mistakes = 0 
 total = len(game_words)
